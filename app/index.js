6514e8826908af4b203f6bfb19c29e556bd91b31
Jhonny <jjimenezz@ucenfotec.ac.cr>
2020-04-06 19:27:56 -0600

    index set up middleware(request to json, static files) & util method getMainDirectory fix

--- app/index.js
+++ app/index.js
@@ -1,16 +1,21 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const getRoadviserDbConnection = require('./util/database');
+const util = require('./util/util');
 
 const port = 8082;
 
+app.use(express.static(path.join(util.getMainDirectory, 'public')));
+app.use(express.json());
+// app.use(express.bodyParser({ extended: false }));
 
 
 getRoadviserDbConnection()
     .then((res) => {
-        console.log(res, '\n', 'Connection establish with Roadvisor data base!');
+        console.log('Connection establish with Roadvisor data base!');
         app.listen(port, () => {
             console.log('Server is running on port: ' + port);
         });
