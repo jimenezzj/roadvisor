400902c265a3702fd79221ab9bfc14ae0d20ed4d
Jhonny <jjimenezz@ucenfotec.ac.cr>
2020-04-07 20:36:44 -0600

    Initial VEHICLE model created, controller to add v1.0, images funcionality added

---
+++ app/controllers/vehicles.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const Vehicle = require('../models/vehicle');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: './../public/assets/images/vehiclesPictures',
+    filename: (req, file, cb) => {
+        const pictureName = new Date().toISOString.split('T')[0] + '_' + req.numeroPlaca + file.originalname;
+        cb(null, pictureName);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+router.post('/add', upload.fields('vehiclesPictures'), (req, res) => {
+    const { numeroPlaca, model, anio, color, type } = req.body;
+    const vehiclePictures = req.files;
+    const newVehicle = new Vehicle({
+        numeroPlaca: numeroPlaca,
+        model: model,
+        anio: anio,
+        color: color,
+        type: type,
+        fotos: vehiclePictures
+    });
+    return res.status(201).json({
+        statusCode: 201,
+        message: 'Vehicle agregado con exito!',
+        fields: newVehicle
+    });
+});
+
+module.exports = { router };
