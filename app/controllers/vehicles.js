72babcc373cbbadee345f6031a00af33ce7c6f94
Jhonny <jjimenezz@ucenfotec.ac.cr>
2020-04-07 21:09:08 -0600

    fixes to receive & respond with images & rpute vehicles added

--- app/controllers/vehicles.js
+++ app/controllers/vehicles.js
@@ -6,16 +6,18 @@ const multer = require('multer');
 const storage = multer.diskStorage({
     destination: './../public/assets/images/vehiclesPictures',
     filename: (req, file, cb) => {
-        const pictureName = new Date().toISOString.split('T')[0] + '_' + req.numeroPlaca + file.originalname;
+        const pictureName = new Date().toISOString().split('T')[0]
+            + '_' + req.body.numeroPlaca
+            + '_' + file.originalname;
         cb(null, pictureName);
     }
 });
 
 const upload = multer({ storage: storage });
 
-router.post('/add', upload.fields('vehiclesPictures'), (req, res) => {
+router.post('/add', upload.array('vehiclePictures'), (req, res) => {
     const { numeroPlaca, model, anio, color, type } = req.body;
-    const vehiclePictures = req.files;
+    const vehiclePictures = [req.files[0].path];
     const newVehicle = new Vehicle({
         numeroPlaca: numeroPlaca,
         model: model,
@@ -31,4 +33,4 @@ router.post('/add', upload.fields('vehiclesPictures'), (req, res) => {
     });
 });
 
-module.exports = { router };
+module.exports = router;
