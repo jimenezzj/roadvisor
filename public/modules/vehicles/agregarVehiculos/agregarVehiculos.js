be38cf9a148773f92708a47f4c8ea2c33b64e5e9
Jhonny <jjimenezz@ucenfotec.ac.cr>
2020-04-10 17:05:22 -0600

    mark-up v1.0 & upload functionality v1.0

--- public/modules/vehicles/agregarVehiculos/agregarVehiculos.js
+++ public/modules/vehicles/agregarVehiculos/agregarVehiculos.js
@@ -1,4 +1,48 @@
 const mainWrapper = document.querySelector('.wrapperContainer');
+const uploadPicWrapper = document.querySelector('.uploadPicWrapper');
+const picsWrapper = document.querySelector('.picsWrapper');
+const imagesPreview = document.querySelector('.imagesPreview');
+const imageFieldWrapper = document.querySelector('.imageFieldWrapper');
+const btnUploadPicture = document.querySelector('.btnUploadPic');
+const btnAddPic = document.querySelector('.btnAddPic');
+const vehiImagesField = document.querySelector('#vehiImagesField');
+const picsList = document.querySelector('.picsList');
+
+let currentFiles = [];
+
+const updateCurrentFiles = (filesUpload) => {
+    currentFiles = [...currentFiles, ...filesUpload];
+    currentFiles.forEach(file => {
+        picsList.appendChild(
+            createListItem(file.name)
+        );
+    })
+    console.log(filesListElements);
+};
+
+btnUploadPicture.addEventListener('click', () => {
+    vehiImagesField.click();
+});
+
+btnAddPic.addEventListener('click', () => {
+    vehiImagesField.click();
+});
+
+vehiImagesField.addEventListener('change', (e) => {
+    // console.dir(e.target);
+    const filesLenght = e.target.files;
+    if (filesLenght) {
+        uploadPicWrapper.classList.add('uploadPicWrapper--hide');
+        picsWrapper.classList.add('picsList--show');
+        imagesPreview.classList.add('picsList--show');
+        imageFieldWrapper.classList.add('imageFieldWrapper--full');
+    } else {
+        uploadPicWrapper.classList.remove('uploadPicWrapper--hide');
+        picsWrapper.classList.remove('picsList--show');
+        imagesPreview.classList.remove('picsList--show');
+    }
+    updateCurrentFiles(filesLenght);
+});
 
 mainWrapper.querySelector('header').appendChild(
     createSidebar([
@@ -41,3 +85,26 @@ mainWrapper.querySelector('main').insertBefore(
     document.querySelector('.breadCrumb')
 );
 
+const createListItem = (picName) => {
+    const picItemElement = document.createElement('div');
+    const iconElement = document.createElement('i');
+    iconElement.classList.add('material-icons');
+    const picNameElement = document.createElement('p');
+    const xIcon = iconElement.cloneNode(true);
+    const imageIcon = iconElement.cloneNode(true);
+    picItemElement.classList.add('picsList__item');
+    picNameElement.innerHTML = picName;
+    xIcon.innerHTML = 'insert_photo';
+    imageIcon.innerHTML = 'close';
+    picItemElement.appendChild(xIcon);
+    picItemElement.appendChild(picNameElement);
+    picItemElement.appendChild(imageIcon);
+    return picItemElement.cloneNode(true);
+}
+/*
+                            <div class="picsList__item">
+                                <i class="material-icons">insert_photo</i>
+                                <p>Nombre de foto.jpg</p>
+                                <i class="material-icons">close</i>
+                            </div>
+*/
